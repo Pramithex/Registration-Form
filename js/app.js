2bc62cde09ef2d693009b6cb60f3dba17b539525
pramithex <mitpramith@gmail.com>
2016-11-14 22:03:46 +0530

    Initial files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myRegApp', [ngRoute]);
+
+/*myApp.controller('regAppController', ['$scope', function($scope){
+	$scope.message="Welcome to access";  
+}])*/
+
+myRegApp.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.
+		When('/login',{
+			templateUrl:'views/login.html',
+			Controller:'RegistrationController'
+		}).
+		When('/register',{
+			templateUrl:'views/register.html',
+			Controller:'RegistrationController'
+		}).
+		When('/success',{
+			templateUrl:'views/success.html',
+			Controller:'SuccessController'
+		}).
+		otherwise({
+			redirectTo:'/login'
+		});
+}]);
